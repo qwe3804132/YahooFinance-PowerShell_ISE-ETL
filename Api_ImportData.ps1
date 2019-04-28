ad1598f3e6299bd691d26c6a057e892a17b620c0
qwe3804132 <liupeiran9324@outlook.com>
2019-04-28 17:51:12 -0400

    Create Api_ImportData.ps1
    The ETL scripts that loading through API port then write to existed database table

---
+++ Api_ImportData.ps1
@@ -0,0 +1,42 @@
+﻿Function Get-BitstampData {
+    Param(
+    [string]$connectionstring
+    )
+    Process
+    {
+
+        while ($x = 1)
+        {
+
+            $w = (New-Object net.webclient).DownloadString("https://www.bitstamp.net/api/ticker/")
+            $p = $w | ConvertFrom-Json
+
+            $last = $p.last
+            $timestamp = $p.timestamp
+            $volume = $p.volume
+
+            $scon = New-Object System.Data.SqlClient.SqlConnection
+            $scon.ConnectionString = "$connectionstring"
+
+            
+            $cmd = New-Object System.Data.SqlClient.SqlCommand
+            $cmd.Connection = $scon
+            $cmd.CommandText = "INSERT INTO BitstampTable VALUES (@1, @2, @3)"
+            $cmd.Parameters.Add("@1", $last)
+            $cmd.Parameters.Add("@2", $timestamp)
+            $cmd.Parameters.Add("@3", $volume)
+            
+            $scon.Open()
+            $cmd.ExecuteNonQuery() | Out-Null
+            $scon.Dispose()
+
+            Start-Sleep -s 20
+
+        }
+    }
+}
+
+###  In the parameter for connection string, put the connection string of your server/database
+# FORMAT (if using Windows Authentication): Data Source=MYSERVER\MYINSTANCE;Initial Catalog=MyDatabase;Integrated Security=true
+
+Get-BitstampData -connectionstring "Data Source=PEIRANLIU17B7;Initial Catalog=ETLCourse;Integrated Security=true"
