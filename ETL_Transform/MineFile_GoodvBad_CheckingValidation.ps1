8225032480cf202b2dc5e5313375fbab4345315c
qwe3804132 <liupeiran9324@outlook.com>
2019-05-01 18:55:05 -0400

    Create MineFile_GoodvBad_CheckingValidation.ps1

---
+++ ETL_Transform/MineFile_GoodvBad_CheckingValidation.ps1
@@ -0,0 +1,35 @@
+﻿Function MineFile_GoodvBad ($file, $resultfiles, $goodCount)
+{
+    $read = New-Object System.IO.StreamReader($file)
+    $goodfile = $resultfiles + "good.txt"
+    $badfile = $resultfiles + "bad.txt"
+    New-Item $goodfile -ItemType file
+    New-Item $badfile -ItemType file
+    $lineNo = 0
+    $count = 0
+    $goodA = @()
+    $badA = @()
+
+    while ($line = $read.ReadLine())
+    {
+         $count = $line.Split(',').Length -1
+     
+         if ($count -ne $goodCount)
+         {
+            $badA += $line
+         }
+         else
+         {
+            $goodA += $line
+         }
+
+         $lineNo++
+    }
+    $read.Close()
+    $read.Dispose()
+
+    ##$goodA
+    $badA
+}
+
+MineFile_GoodvBad -file "C:\Users\Timothy Smith\Desktop\udmps\files\ETL_Transform\ourfile.txt" -resultfiles "C:\Users\Timothy Smith\Desktop\udmps\files\ETL_Transform\log\" -goodCount 2
