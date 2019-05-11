d2b714b5a677d2a86e811d83a99320cccae82917
qwe3804132 <liupeiran9324@outlook.com>
2019-05-11 15:20:56 -0400

    Create stripchars.ps1

---
+++ Master_Extract/stripchars.ps1
@@ -0,0 +1,49 @@
+﻿Function SeparateGoodBadLines_Strip ($file, $ch, [string[]]$strip)
+{
+    $ext = $file.Substring($file.LastIndexOf("."))
+    $loc = $file.Substring(0,($file.LastIndexOf("\")+1))
+    $name = $file.Substring($file.LastIndexOf("\")+1).Replace($ext,"")
+    
+    $valid = Get-Content $file | Select -First 1
+    $validCount = $valid.Split($ch).Length - 1
+ 
+    $valid = $loc + $name + "_valid" + $ext
+    $invalid = $loc + $name + "_invalid" + $ext
+ 
+    New-Item $valid -ItemType file
+    New-Item $invalid -ItemType file
+ 
+    $read = New-Object System.IO.StreamReader($file)
+    $wValid = New-Object System.IO.StreamWriter($valid)
+    $wInvalid = New-Object System.IO.StreamWriter($invalid)
+ 
+ 
+    while (($line = $read.ReadLine()) -ne $null)
+    {
+        $total = $line.Split($ch).Length - 1
+        if ($total -eq $validCount)
+        {
+            foreach ($st in $strip)
+            {
+                $line = $line.Replace("$st","")
+            }
+            $wValid.WriteLine($line)
+            $wValid.Flush()
+        }
+        else
+        {
+            $wInvalid.WriteLine($line)
+            $wInvalid.Flush()
+        }
+    }
+ 
+ 
+    $read.Close()
+    $read.Dispose()
+    $wValid.Close()
+    $wValid.Dispose()
+    $wInvalid.Close()
+    $wInvalid.Dispose()
+}
+ 
+SeparateGoodBadLines -file "C:\files\dirtyfile.txt" -ch "," -strip @("$","*","!","#")
