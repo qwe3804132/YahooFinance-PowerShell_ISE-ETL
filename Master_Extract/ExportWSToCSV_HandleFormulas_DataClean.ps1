d791ffaa7269b90aaa0dff571c95fc57e89ecb1c
qwe3804132 <liupeiran9324@outlook.com>
2019-05-12 21:11:39 -0400

    Create ExportWSToCSV_HandleFormulas_DataClean.ps1
    Handing function with Excel

---
+++ Master_Extract/ExportWSToCSV_HandleFormulas_DataClean.ps1
@@ -0,0 +1,43 @@
+﻿Function ExportWSToCSV_Clean ($excelFileName, $csvLoc)
+{
+    $excelFile = "C:\files\Excel\" + $excelFileName + ".xlsx"
+    $E = New-Object -ComObject Excel.Application
+    $E.Visible = $false
+    $E.DisplayAlerts = $false
+    $wb = $E.Workbooks.Open($excelFile)
+    foreach ($ws in $wb.Worksheets)
+    {
+        $maxrow = $ws.UsedRange.Cells | Select-Object -Last 1 | Select-Object -Property Row
+        $maxrow = $maxrow.Row
+        $maxcol = $ws.UsedRange.Cells | Select-Object -Last 1 | Select-Object -Property Column
+        $maxcol = $maxcol.Column
+
+        for ($c = 1; $c -le $maxcol; $c++)
+        {
+            for ($r = 1; $r -le $maxrow; $r++)
+            {
+                if ($ws.Cells.Item($r,$c).HasFormula -eq $False)
+                {
+                    if ($ws.Cells.Item($r,$c).Value() -like "*,*")
+                    {
+                        $ws.Cells.Item($r,$c).Value() = $ws.Cells.Item($r,$c).Value().Replace(",","")
+                    }
+                    elseif (($ws.Cells.Item($r,$c).Value()*0) -eq 0)
+                    {
+                        $ws.Cells.Item($r,$c).NumberFormat ="0"
+                    }
+                }
+            }
+        }
+        $n = $excelFileName + "_" + $ws.Name
+        $ws.SaveAs($csvLoc + $n + ".csv", 6)
+    }
+    $E.Quit()
+ 
+    if (Get-Process -ProcessName EXCEL)
+    {
+        Stop-Process -ProcessName EXCEL
+    }
+}
+ 
+ExportWSToCSV_Clean -excelFileName "functions" -csvLoc "C:\files\Excel\CSV\"
